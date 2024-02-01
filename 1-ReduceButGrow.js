0fc2d15d4d10a5bb2f6ed724f26d5b4a1b13aabc
heru <jrs.herupranata@gmail.com>
2024-02-01 11:33:07 +0700

    1/366

---
+++ 1-ReduceButGrow.js
@@ -0,0 +1,24 @@
+// function grow(x) {
+//   let result = x[0];
+//   for (let i = 1; i < x.length; i++) {
+//     result = result * x[i];
+//   }
+//   return result;
+// }
+
+// ! IMMUTABILITY REDUCE
+// function grow(x) {
+//   const result = x.reduce(
+//     (previousValue, currentValue) => previousValue * currentValue,
+//     1
+//   );
+//   return result;
+// }
+
+// ! FUNCTION EXPRESSION
+const grow = (x) =>
+  x.reduce((previousValue, currentValue) => previousValue * currentValue, 1);
+
+console.log(grow([1, 2, 3]));
+console.log(grow([4, 1, 1, 1, 4]));
+console.log(grow([2, 2, 2, 2, 2, 2]));
